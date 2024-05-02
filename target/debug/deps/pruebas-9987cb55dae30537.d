03d032ad2b892c6e4c96fc45c07bf29a3d7156e5
acronis333 <acronis333@gmail.com>
2024-05-02 11:39:44 +0200

    add new

---
+++ target/debug/deps/pruebas-9987cb55dae30537.d
@@ -0,0 +1,5 @@
+/home/javier/code/rust/pruebas/target/debug/deps/pruebas-9987cb55dae30537: src/main.rs
+
+/home/javier/code/rust/pruebas/target/debug/deps/pruebas-9987cb55dae30537.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/pruebas.d
@@ -0,0 +1,1 @@
+/home/javier/code/rust/pruebas/target/debug/pruebas: /home/javier/code/rust/pruebas/src/main.rs
